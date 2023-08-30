10c25f2d2a90cda908d7001d2312c7b8d0bd8807
Ngaishin <m2498648328@gmail.com>
2023-08-30 18:46:57 +0800

    MyCLearningProcess

---
+++ Head.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//number of daffodils
+void flower(int n, int m);
+void flower_input();
+
+//Switch
+void Switch();
+
+//input account
+void doWhile();
+
+//buy chicken
+void buyChicken();
+
+//print rhombus
+void prnRhombus(int);
+
+//find number
+void findNum(int* arr, int arrLength, char numType);
+
+//selection sort
+void selSort(int *arr,int arrLen);
+
+//Binary search
+void binSearch(int *arr,int arrLen,int sch);
